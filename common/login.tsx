7ff4309ff28ede0d488fdc1b072554e0cb388ce0
ByCold <49861259+ByCold@users.noreply.github.com>
2025-07-01 11:54:44 +0800

    Update login.tsx

--- common/login.tsx
+++ common/login.tsx
@@ -72,8 +72,8 @@ export default function PlatformLoginForm() {
     <LoginFormPage
       backgroundImageUrl="/backgroundImage.png"
       logo="/logo.png"
-      title="One XSS"
-      subTitle="用于渗透测试人员使用的XSS平台"
+      title="洞鉴XSS"
+      subTitle="网络安全测试"
       onFinish={handleSubmit}
     >
       <Tabs defaultActiveKey='0' items={tab_items} onChange={(activeKey) => setLoginType(activeKey as LoginType)} />
